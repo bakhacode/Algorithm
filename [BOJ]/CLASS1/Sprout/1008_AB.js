38135550a3ffe1ca143ebbee8b52b42e36ff6162
bakhacode <bakhamitai@naver.com>
2023-01-08 03:06:58 +0900

    [BOJ]Bronze5/A/B/1m/23-01-08

---
+++ [BOJ]/CLASS1/Sprout/1008_AB.js
@@ -0,0 +1,13 @@
+//[입출력과 사칙연산]
+//[BOJ]Bronze5/A/B/1m/23-01-08
+//https://www.acmicpc.net/problem/1008
+
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+let input = fs.readFileSync(file).toString().split(' ').map(Number);
+
+let [a, b] = input
+
+let result = a / b
+
+console.log(result);
