6d82041ebd9ed889dc512af00ec119c8a781df4a
bakhacode <bakhamitai@naver.com>
2023-01-08 03:49:45 +0900

    [BOJ]Bronze5/고양이/5m/23-01-08

---
+++ [BOJ]/CLASS1/Sprout/10171_고양이.js
@@ -0,0 +1,12 @@
+//[출력]
+//[BOJ]Bronze5/고양이/5m/23-01-08
+//https://www.acmicpc.net/problem/10171
+
+console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`)
+
+
+//이것도 어이없는 문제인데 백틱이랑 백슬래시 출력등 필요한 부분 알게됨 ㅋㅋ 아래는 참고한 블로그
+//https://velog.io/@xoxobabegirl/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-Node.js-%EB%B0%B1%EC%A4%80-10171-10172-%EB%AC%B8%EC%A0%9C
